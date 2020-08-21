4acd7555ec30244a6cfd0a00a591138726d33adc
Cosmas Nyairo <nyairocosmas@gmail.com>
2020-08-21 14:04:01 +0300

    Add blockchain and node classes

---
+++ blockchain/classes/node.py
@@ -0,0 +1,97 @@
+from uuid import uuid4
+
+from blockchain import Blockchain
+from verification import Verification
+
+
+class Node:
+
+    def __init__(self):
+        #self.id = str(uuid4())
+        self.id = 'ABC'
+        self.blockchain = Blockchain(self.id)
+
+    def get_transaction(self):
+        tx_recipient = input('Enter recipeint of transaction: ')
+
+        # visit_time = float(input('Time of visit:'))
+        medical_notes = input('Enter Medical notes: ')
+        diagnosis = input('Enter Diagnosis: ')
+        # split converts to a list
+        prescription = [input('Enter Prescription(s): ')]
+        lab_results = input('Enter lab result(s): ')
+
+        tx_transaction_details = {
+            # 'doctor_name':
+            'medical_notes': medical_notes,
+            'diagnosis': diagnosis,
+            'prescription': prescription,
+            'lab_results': lab_results
+        }
+        return tx_recipient, tx_transaction_details
+
+    def get_choice(self):
+        """Gets choice from the user."""
+        user_input = input('Your choice: ')
+        return user_input
+
+    def print_blockchain(self):
+        """ Output blocks of the blockchain. """
+
+        for block in self.blockchain.get_chain():
+            print('Outputting the blockchain')
+            print(block)
+        else:
+            print('-' * 20)
+
+    def listen_for_input(self):
+        user_inputted = True
+
+        while user_inputted:
+            print('Please choose')
+            print('1: Add new transaction')
+            print('2: Mine a new block')
+            print('3: Output blocks')
+            print('e: Exit')
+
+            user_choice = self.get_choice()
+
+            if user_choice == '1':
+                tx_data = self.get_transaction()
+                recipient, transaction_details = tx_data
+                self.blockchain.add_transaction(
+                    recipient,
+                    self.id,
+                    details=transaction_details
+                )
+                print(self.blockchain.get_open_transactions())
+
+            elif user_choice == '2':
+                self.blockchain.mine_block()
+
+            elif user_choice == '3':
+                self.print_blockchain()
+
+            elif user_choice == 'e':
+                # exits from the blockchain
+                print('Exited from the blockchain')
+                print('Blockchain is : ', self.blockchain)
+                user_inputted = False
+
+            else:
+                print('Input was invalid, please pick a value from the list!')
+
+            if not Verification.verify_chain(self.blockchain.get_chain()):
+                self.print_blockchain()
+                print('Blockchain is invalid!')
+                break
+
+        else:
+            print('User left!')
+            print('-' * 20)
+
+        print('Bye!')
+
+
+node = Node()
+node.listen_for_input()
