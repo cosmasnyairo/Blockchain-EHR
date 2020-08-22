6653fc9420edfd85f2b58caa67163cd9f40a3712
Cosmas Nyairo <nyairocosmas@gmail.com>
2020-08-22 09:09:17 +0300

    create wallet class

---
+++ blockchain/wallet.py
@@ -0,0 +1,25 @@
+from Crypto.PublicKey import RSA
+import Crypto.Random
+
+import binascii
+
+print('dna')
+
+
+class Wallet:
+    def __init__(self):
+        private_key, public_key = self.generate_keys()
+
+        self.private_key = private_key
+        self.public_key = public_key
+
+    def generate_keys(self):
+        private_key = RSA.generate(1024, Crypto.Random.new().read)
+        public_key = private_key.publickey()
+        print(private_key, public_key)
+        return(
+            binascii.hexlify(private_key.exportKey(
+                format='DER')).decode('ascii'),
+            binascii.hexlify(public_key.exportKey(
+                format='DER')).decode('ascii')
+        )
