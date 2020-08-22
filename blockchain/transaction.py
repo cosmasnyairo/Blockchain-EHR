b78c507900dce0a0b596a8dd79dfffe41b4127a7
Cosmas Nyairo <nyairocosmas@gmail.com>
2020-08-22 12:12:50 +0300

    Adding signatures

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -2,10 +2,11 @@ from collections import OrderedDict
 
 
 class Transaction():
-    def __init__(self, sender, recepient, details):
+    def __init__(self, sender, recepient, signature, details):
         self.sender = sender
         self.receiver = recepient
         self.details = details
+        self.signature = signature
 
     # create ordered dict to help in generating guess in valid proof
     def to_ordered_dict(self):
