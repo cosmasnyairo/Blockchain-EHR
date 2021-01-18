a863d8a6c742ee0b1f1902633c25962ab762eec9
Cosmas Nyairo <nyairocosmas@gmail.com>
2021-01-18 21:40:55 +0300

    Create frontend ui for blockchain peers

---
+++ blockchain/Frontend.py
@@ -0,0 +1,76 @@
+import socket
+import subprocess
+import tkinter as tk
+from tkinter.constants import *
+import tkinter.font as tkFont
+
+
+class App:
+    def __init__(self, root):
+        self.get_ip()
+        root.wm_title('EHR | Blockchain Peers Panel')
+        root.resizable(False, False)
+
+        # setting window size
+        width, height = 450, 240
+        difwidth, difheight = (
+            (root.winfo_screenwidth()-width)/2), ((root.winfo_screenheight()-height)/2)
+        alignstr = '%dx%d+%d+%d'.format() % (width, height, difwidth, difheight)
+        root.geometry(alignstr)
+
+        peer_btn = tk.Label(root)
+        self.format_widget(peer_btn, None,
+                           "Click button below to start node", label=True, width=30)
+        peer_btn.pack(side=tk.TOP, padx=10, pady=10)
+
+        peers = [5000, 5001]
+        for peer in peers:
+            frame = tk.Frame(root)
+            frame.pack()
+
+            peer_label = tk.Label(frame)
+            self.format_widget(peer_label, "#a0a0a0",
+                               "Node {}".format(peer), label=True)
+            peer_label.pack(side=tk.LEFT, padx=10, pady=10)
+
+            peer_btn = tk.Button(frame)
+            self.format_widget(peer_btn, "#000000",
+                               "Start Node", command=lambda peer=peer: self.onclick(peer))
+            peer_btn.pack(side=tk.LEFT, padx=10, pady=15)
+
+        bottomframe = tk.Frame(root)
+        bottomframe.pack()
+        btn_ipadr = tk.Button(bottomframe)
+        self.format_widget(btn_ipadr, "#000000",
+                           'Refresh', command=lambda: self.get_ip())
+        btn_ipadr.pack(side=tk.BOTTOM, padx=5, pady=5)
+
+    def onclick(self, peer):
+        subprocess.Popen(
+            "python node.py -p {}".format(peer), shell=True)
+        # print('output is ', subprocess.check_output([command]))
+
+    def format_widget(self, Widget, color, text, command=None, label=False, width=None):
+        Widget["bg"] = color
+        Widget["font"] = tkFont.Font(family='Monteserrat', size=10)
+        Widget["text"] = text
+        Widget["fg"] = "#ffffff"
+        if label == True:
+            Widget["fg"] = "#000000"
+            Widget['bd'] = 5
+            Widget["width"] = 20 if width == None else width
+        else:
+            Widget["width"] = 15
+            Widget["command"] = command
+
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        self.ip_address = s.getsockname()[0]
+        s.close()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
